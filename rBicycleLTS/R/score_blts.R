b975d72be4238f80467b8ff52f9cc64a136205a5
Bikingman <daniel.patterson87@gmail.com>
2023-02-01 10:09:49 -0500

    add src

--- rBicycleLTS/R/score_blts.R
+++ rBicycleLTS/R/score_blts.R
@@ -45,7 +45,6 @@ get_class <- function(column){
   return(config[[column]]$datatype)
 }
 
-
 get_default_class <- function(column) {
   return(config[[column]]$default_datatype)
 }
@@ -53,9 +52,13 @@ get_default_class <- function(column) {
 add_default_columns <- function(df, config) {
   for (i in seq_len(length(config$segment))) {
     name <- config$segment[[i]]$name
+    if (!any(names(df) %in% paste0(name, '_src'))){
+      df[[paste0(name, '_src')]]
+    }
     if (!any(names(df) %in% name)) {
       df[[name]] <- NA
     }
+
   }
   return(df)
 }
@@ -78,17 +81,18 @@ assign_classes <- function(df, config) {
 
 define_assumptions <- function(df, config) {
   for (i in seq_len(length(config$segment) - 1)) {
+
     name <- config$segment[[i]]$name
+    df[[paste0(name, '_src')]] <- ifelse(is.na(df[[name]]),  'assumed', 'source_data')
     if ("assumptions" %in% names(config$segment[[i]])) {
       for (j in seq_len(length(config$segment[[i]]$assumptions))) {
         val <- config$segment[[i]]$assumptions[j][[1]]$val
         if (!(j == length(config$segment[[i]]$assumptions))) {
           condition <- config$segment[[i]]$assumptions[j][[1]]$where
           df[[name]] <- ifelse(is.na(df[[name]]) & 
-                                 eval(parse(text = condition)), 
-                               val,
-                               df[[name]]
-          )
+                               eval(parse(text = condition)), 
+                               val, 
+                               df[[name]])
         } else {
           df[[name]] <- ifelse(is.na(df[[name]]), val, df[[name]])
         }}}}
@@ -113,7 +117,6 @@ detect <- function(x, y){
 }
 
 fix_speed <- function(roads, maxspeedlimit_col) {
-
   roads[[maxspeedlimit_col]] <- 5*(as.integer(roads[[maxspeedlimit_col]])%/%5 + as.logical(as.integer(roads[[maxspeedlimit_col]])%%5))
   return(roads)
 }
@@ -129,54 +132,54 @@ perform_lanes_check <- function(df, lanes, centerline){
 
 score_blts_or <- function(df, config, scores ) {
 
-    df <- add_default_columns(df, config)
-    df <- assign_classes(df, config)
-    df <- define_assumptions(df, config)
-    df <- convert_title(df)
-    df <- fix_speed(df, 'maxspeed')
+  df <- add_default_columns(df, config)
+  df <- assign_classes(df, config)
+  df <- define_assumptions(df, config)
+  df <- convert_title(df)
+  df <- fix_speed(df, 'maxspeed')
+  
+  for (i in seq_len(length(config$segment))){
+    assign(names(config$segment[i]), config$segment[[i]]$name)
+  }
+  
+  bike_facs <- c()
+  for (i in seq_len(length(config$segment$bike_infra$vals))){
+    bike_facs <- append(bike_facs, config$segment$bike_infra$vals[[i]])
+    assign(names(config$segment$bike_infra$vals)[[i]], config$segment$bike_infra$vals[[i]])
+  }
+  
+  blts_no_bike_lane <- scores %>% filter(bike_lane == 'No')
+  blts_bike_lane <- scores %>% filter(bike_lane == 'Yes')
 
-
-    for (i in seq_len(length(config$segment))){
-        assign(names(config$segment[i]), config$segment[[i]]$name)
-    }
-
-    bike_facs <- c()
-    for (i in seq_len(length(config$segment$bike_infra$vals))){
-        bike_facs <- append(bike_facs, config$segment$bike_infra$vals[[i]])
-        assign(names(config$segment$bike_infra$vals)[[i]], config$segment$bike_infra$vals[[i]])
-    }
-
-    blts_no_bike_lane <- scores %>% filter(bike_lane == 'No')
-    blts_bike_lane <- scores %>% filter(bike_lane == 'Yes')
-    df1 <- fuzzy_left_join(df[!(df[[bike_infra]] %in%  bike_facs), ], blts_no_bike_lane,
-                            by = c( fclass = "func_class",
-                                    maxspeed = "lower_speed",
-                                    maxspeed = "upper_speed",
-                                    lanes = "lower_lanes",
-                                    lanes = "upper_lanes",
-                                    centerline = "centerline",
-                                    aadt = "lower_adt",
-                                    aadt = "upper_adt"
-                            ),
-                            match_fun = list( detect, `>=`, `<=`,  `>=`, `<=`, `==`, `>=`, `<=` )
-    )
-
-    df2 <- fuzzy_left_join(df[df[[bike_infra]] %in%  bike_facs,], blts_bike_lane,
-                            by = c(  
-                            maxspeed = "lower_speed",
-                            maxspeed = "upper_speed",
-                            lanes = "lower_lanes",
-                            lanes = "upper_lanes",
-                            parking = "parking",
-                            bike_l_width = "lower_bl_width",
-                            bike_l_width = "upper_bl_width"
-                            ),
-                            match_fun = list( `>=`, `<=`,  `>=`, `<=`,  `==`, `>=`, `<=` )
-    )
-
-    df3 <- rbind(df1, df2)
-    df3$blts_score <- ifelse(
-        df3[[bike_infra]] %in% c(protected_bl, shared_use_path), 1, df3$blts_score 
-    )
+  df1 <- fuzzy_left_join(df[!(df[[bike_infra]] %in%  bike_facs), ], blts_no_bike_lane,
+                         by = c( fclass = "func_class",
+                                 maxspeed = "lower_speed",
+                                 maxspeed = "upper_speed",
+                                 lanes = "lower_lanes",
+                                 lanes = "upper_lanes",
+                                 centerline = "centerline",
+                                 aadt = "lower_adt",
+                                 aadt = "upper_adt"
+                         ),
+                         match_fun = list( detect, `>=`, `<=`,  `>=`, `<=`, `==`, `>=`, `<=` )
+  )
+  
+  df2 <- fuzzy_left_join(df[df[[bike_infra]] %in%  bike_facs,], blts_bike_lane,
+                         by = c(  
+                           maxspeed = "lower_speed",
+                           maxspeed = "upper_speed",
+                           lanes = "lower_lanes",
+                           lanes = "upper_lanes",
+                           parking = "parking",
+                           bike_l_width = "lower_bl_width",
+                           bike_l_width = "upper_bl_width"
+                         ),
+                         match_fun = list( `>=`, `<=`,  `>=`, `<=`,  `==`, `>=`, `<=` )
+  )
+  
+  df3 <- rbind(df1, df2)
+  df3$blts_score <- ifelse(
+    df3[[bike_infra]] %in% c(protected_bl, shared_use_path), 1, df3$blts_score 
+  )
   return(df3)
 }
